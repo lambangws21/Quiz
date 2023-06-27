a5e2b634da0897e428dcb0209f650eba61ed1b3b
lambangws21 <lambangws21@gmail.com>
2023-06-27 13:24:37 +0700

    Menambahkan script

---
+++ script.js
@@ -0,0 +1,32 @@
+const questions = [
+    {
+        question : "Siapa nama Presiden Indonesia saat ini?"
+        answers: [
+            {text: "Bapak Satria, correct:false"},
+            {text: "Bu Medi, correct:false"},
+            {text: "Bapak Jokowi, correct:true"},
+            {text: "Bapak Agus, correct:false"},
+        ]
+    },
+    {
+        question : "Siapa nama Presiden Indonesia saat ini?"
+        answers: [
+            {text: "Bapak Satria, correct:false"},
+            {text: "Bu Medi, correct:false"},
+            {text: "Bapak Jokowi, correct:true"},
+            {text: "Bapak Agus, correct:false"},
+        ]
+    },
+    {
+        question : "Siapa nama Presiden Indonesia saat ini?"
+        answers: [
+            {text: "Bapak Satria, correct:false"},
+            {text: "Bu Medi, correct:false"},
+            {text: "Bapak Jokowi, correct:true"},
+            {text: "Bapak Agus, correct:false"},
+        ]
+    },
+];
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answe-buttons");
+const nextButton = document.getElementById("next-btn");
